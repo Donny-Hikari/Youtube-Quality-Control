665628697597a8961b49327fff40fa1f8485dfb7
Donny Hikari <donny.hikari@gmail.com>
2024-05-19 07:16:10 +0900

    Add playback speed control. Enhance robostness.

--- pages/popup.js
+++ pages/popup.js
@@ -2,24 +2,28 @@
 // Youtube Quality Control
 // By Donny
 // Donny (c) 2020
-// 
+//
 
 const allPossibleQualities = ["best", "4320p", "2160p", "1440p", "1080p", "720p", "480p", "360p", "240p", "144p"]
+const allPossiblePlaybackSpeed = ['0.25', '0.5', '0.75', '1.0', '1.25', '1.5', '1.75', '2']
 
 const storage = chrome.storage.sync
 
 let currentQuality = "best"
+let currentSpeed = '1.0'
 let notPremium = true
 let skipAd = true
 let swapColumns = false
 
-storage.get(['quality', 'skipAd', 'swapColumns'], function (d) {
+storage.get(['quality', 'skipAd', 'swapColumns', 'notPremium', 'playbackSpeed'], function (d) {
     currentQuality = d['quality'] || "best"
+    currentSpeed = d['playbackSpeed'] || '1.0'
     notPremium = d['notPremium'] ?? true
     skipAd = d['skipAd'] ?? true
     swapColumns = d['swapColumns'] ?? false
 
     setupQualityList()
+    setupPlaybackSpeedList()
     setupNotPremium()
     setupSkipAd()
     setupSwapColumns()
@@ -43,7 +47,7 @@ function setupQualityList() {
                 console.log(`Upper limit of quality now changed to ${q}.`)
             })
         })
-    
+
         let label = document.createElement("label")
         if (q === "best") {
             label.innerText = "Best possible"
@@ -51,10 +55,43 @@ function setupQualityList() {
             label.innerText = q
         }
         label.setAttribute('for', `quality-${q}`)
-    
+
         itemWrapper.append(item)
         itemWrapper.append(label)
         qualityList.append(itemWrapper)
+    }
+}
+
+function setupPlaybackSpeedList() {
+    let speedList = document.querySelector(".speed-list")
+    for (let q of allPossiblePlaybackSpeed) {
+        let itemWrapper = document.createElement("div")
+
+        let item = document.createElement("input")
+        item.setAttribute('id', `speed-${q}`)
+        item.setAttribute('type', "radio")
+        item.setAttribute('name', "speed")
+        item.setAttribute('value', q)
+        if (q === currentSpeed) {
+            item.checked = true
+        }
+        item.addEventListener('change', () => {
+            storage.set({playbackSpeed: q}, function () {
+                console.log(`Default playback speed now changed to ${q}.`)
+            })
+        })
+
+        let label = document.createElement("label")
+        if (q === "1.0") {
+            label.innerText = "Normal"
+        } else {
+            label.innerText = q
+        }
+        label.setAttribute('for', `speed-${q}`)
+
+        itemWrapper.append(item)
+        itemWrapper.append(label)
+        speedList.append(itemWrapper)
     }
 }
 
--- scripts/qualityctl.js
+++ scripts/qualityctl.js
@@ -2,11 +2,15 @@
 // Youtube Quality Control
 // By Donny
 // Donny (c) 2020
-// 
+//
 const $=document.querySelector.bind(document)
 const LOG_PREFIX = "[Donny Youtube Quality Control] "
+const LOG_LEVEL = 'debug'
 const QUALITYCTRL_LABELS = ["Quality", "画質", "画质"]
-const maxTrials4Quality = 20
+const MUTED_LABELS = ["ミュート解除"]
+const PLAYBACK_SPEED_LABELS = ["Playback speed", "再生速度"]
+const NORMAL_SPEED_LABELS = ["Normal", "標準"]
+const maxTrials4Quality = 100
 
 const storage = chrome.storage.sync
 
@@ -37,15 +41,94 @@ function swapColumnsCtl() {
     }
 }
 
+function isVisible(e) {
+    return e.style.display != 'none';
+}
+
+function openSettingsMenu(open) {
+    open = open ?? true;
+    open = Boolean(open)
+
+    let settingsMenu = $(".ytp-settings-menu")
+    if (isVisible(settingsMenu) == open) {
+        // already in the desired state
+        return true
+    }
+
+    let settingsBtn = $(".ytp-button.ytp-settings-button")
+    if (settingsBtn == null) {
+        // settings button does not exist
+        return false
+    }
+
+    settingsBtn.click()
+    if (isVisible(settingsMenu) == open) {
+        // now in the desired state
+        return true
+    }
+    return false
+}
+
+function goBackSettingsMenu() {
+    let backBtn = $(".ytp-settings-menu .ytp-panel-back-button")
+    if (backBtn != null) {
+        if (LOG_LEVEL == 'debug') {
+            console.log(`${LOG_PREFIX}Go back settings menu`)
+        }
+        backBtn.click()
+        return true
+    } else {
+        if (LOG_LEVEL == 'debug') {
+            console.log(`${LOG_PREFIX}No back button for settings menu`)
+        }
+        return false
+    }
+}
+
+function findSettingsMenuItem(name_candidates) {
+    if (typeof(name_candidates) == "string") {
+        name_candidates = [name_candidates]
+    }
+
+    goBackSettingsMenu() // try going back just in case
+
+    let settingsMenu = $(".ytp-settings-menu")
+    let menuItems = settingsMenu?.querySelectorAll('.ytp-menuitem')
+    if (settingsMenu == null || menuItems == null || menuItems.length == 0) {
+        if (LOG_LEVEL == 'debug') {
+            console.log(`${LOG_PREFIX}activating settings menu`)
+        }
+        openSettingsMenu(true) // try activating the settings menu
+        settingsMenu = $(".ytp-settings-menu")
+        menuItems = settingsMenu?.querySelectorAll('.ytp-menuitem')
+        if (settingsMenu == null) {
+            if (LOG_LEVEL == 'debug') {
+                console.log(`${LOG_PREFIX}does not work`)
+            }
+            return null
+        } else {
+            setTimeout(() => openSettingsMenu(false), 100)
+        }
+    }
+
+    for (let child of menuItems) {
+        let label = child.querySelector('.ytp-menuitem-label').textContent
+        if (name_candidates.includes(label)) {
+            return child
+        }
+    }
+    return null
+}
+
+
+let startPlayingHandlers = []
+
 let skipAd = true
 let skippedAdsCount = 0
 let skipAdsTimer = null
 function skipAdCtl() {
     // Evil Stuff
-    let adSkipBtn = $(".ytp-ad-skip-button.ytp-button")
-    if (adSkipBtn == null) {
-        adSkipBtn = $(".ytp-ad-skip-button-modern.ytp-button")
-    }
+    let adSkipBtn = $(".ytp-ad-skip-button.ytp-button") || $(".ytp-ad-skip-button-modern.ytp-button") || $(".ytp-skip-ad-button")
     if (skipAd && adSkipBtn != null) {
         adSkipBtn.click()
         skippedAdsCount += 1
@@ -58,19 +141,33 @@ function skipAdCtl() {
         skippedAdsCount += 1
         console.log(LOG_PREFIX + "Do some magic!" + (skippedAdsCount > 1 ? ` x${skippedAdsCount}` : ''))
     } else {
+        function isMuted(muteBtn) {
+            for (let label of MUTED_LABELS) {
+                if (muteBtn.dataset['titleNoTooltip'].startsWith(label)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         // Some Ads can not be skipped
-        let adPreview = $(".ytp-ad-preview-container")
+        let adPreview = $(".ytp-preview-ad")
         if (skipAd && adPreview != null && skipAdsTimer == null) {
             skippedAdsCount += 1
             console.log(LOG_PREFIX + "Doing some complex magic!" + (skippedAdsCount > 1 ? ` x${skippedAdsCount}` : ''))
             let muteBtn = $(".ytp-mute-button")
-            muteBtn.click()
+            if (!isMuted(muteBtn)) {
+                muteBtn.click()
+            }
             skipAdsTimer = setInterval(() => {
-                let adPreview = $(".ytp-ad-preview-container")
-                if (adPreview == null) {
-                    muteBtn.click()
+                let adPreview = $(".ytp-preview-ad")
+                if (adPreview == null || !isVisible(adPreview)) {
+                    if (isMuted(muteBtn)) {
+                        muteBtn.click()
+                    }
                     clearInterval(skipAdsTimer)
                     skipAdsTimer = null
+                    skipAdCtl()
                 }
             }, 100)
         }
@@ -96,16 +193,12 @@ function qualityCtl() {
         }
     }
 
-    let settingsBtn = $(".ytp-button.ytp-settings-button")
-    if (settingsBtn == null) {
-        retry()
-        return false
-    }
-    settingsBtn.click()
-
-    let qualityMenu = $(".ytp-panel-menu .ytp-menuitem:last-child")
-    if (qualityMenu == null || !QUALITYCTRL_LABELS.includes(qualityMenu.querySelector(".ytp-menuitem-label").firstChild.textContent)) {
-        settingsBtn.click()
+    let qualityMenu = findSettingsMenuItem(QUALITYCTRL_LABELS)
+    if (!qualityMenu) {
+        // settings menu does not exist, yet
+        if (LOG_LEVEL == 'debug') {
+            console.log(`${LOG_PREFIX}Quality menu not found`)
+        }
         retry()
         return false
     }
@@ -116,7 +209,7 @@ function qualityCtl() {
     while (qualityItem != null) {
         let label = qualityItem.querySelector("span:first-child").firstChild.textContent
         if ((!notPremium || -1 == label.search("Premium")) && parseInt(label.split('p')[0]) <= upperlimit) {
-            let switchedInfo = `${LOG_PREFIX}Auto switched to ${label}`
+            let switchedInfo = `${LOG_PREFIX}Quality switched to ${label}`
 
             qualityItem.click()
             console.log(switchedInfo)
@@ -128,13 +221,76 @@ function qualityCtl() {
         }
         qualityItem = qualityItem.nextSibling
     }
-
-    if (success != true) {
-        // Leave auto
-        settingsBtn.click()
+    if (!success && LOG_LEVEL == 'debug') {
+        console.log(`${LOG_PREFIX}Quality level ${upperlimit} not found`)
     }
 
-    return true
+    setTimeout(() => {
+        let handler_idx = -1
+        function nextHandler() {
+            handler_idx += 1
+            if (handler_idx < startPlayingHandlers.length) {
+                startPlayingHandlers[handler_idx](nextHandler)
+            }
+        }
+
+        goBackSettingsMenu()
+        nextHandler()
+    }, 200)
+
+    return success
+}
+
+let currentSpeed = '1.0'
+function speedCtl(nextHandler) {
+    console.trace()
+    let speedMenu = findSettingsMenuItem(PLAYBACK_SPEED_LABELS)
+    if (!speedMenu) {
+        // settings menu does not exist
+        if (LOG_LEVEL == 'debug') {
+            console.log(`${LOG_PREFIX}Playback speed menu not found`)
+        }
+        return false
+    }
+    speedMenu.click()
+
+    let speedItems = document.querySelectorAll(".ytp-settings-menu .ytp-panel-menu .ytp-menuitem")
+    let targetItem = null
+    let success = false
+    for (let item of speedItems) {
+        let label = item.firstChild.textContent
+        if (currentSpeed != '1.0') {
+            // not "normal" speed
+            if (label == currentSpeed) {
+                targetItem = item
+                break
+            }
+        } else {
+            // "normal" speed
+            if (NORMAL_SPEED_LABELS.includes(label)) {
+                targetItem = item
+                break
+            }
+        }
+    }
+
+    if (targetItem != null) {
+        let label = targetItem.firstChild.textContent
+        let switchedInfo = `${LOG_PREFIX}Playback speed switched to ${label}`
+
+        targetItem.click()
+        console.log(switchedInfo)
+        success = true
+    } else {
+        console.log(`${LOG_PREFIX}Playback speed ${currentSpeed} not found`)
+    }
+
+    setTimeout(() => {
+        goBackSettingsMenu()
+        nextHandler()
+    }, 200)
+
+    return success
 }
 
 let currentAddress = window.location.href
@@ -150,17 +306,21 @@ function mainCtl() {
     setInterval(() => addressChange(qualityCtl), 1000)
 
     if (skipAd) {
-        setInterval(skipAdCtl, 1000)
+        setInterval(skipAdCtl, 400)
     }
 }
 
-storage.get(['quality','skipAd','swapColumns'], function (d) {
-    let currentQuality = d['quality'] || "best"
+storage.get(['quality','skipAd','swapColumns','notPremium','playbackSpeed'], function (d) {
+    // load configs
+    let currentQuality = d['quality'] ?? "best"
     currentUpperLimit = (currentQuality === 'best') ? Infinity : parseInt(currentQuality.split('p')[0])
 
+    currentSpeed = d['playbackSpeed'] ?? '1.0'
     notPremium = d['notPremium'] ?? true
     skipAd = d['skipAd'] ?? true
     swapColumns = d['swapColumns'] ?? false
+
+    startPlayingHandlers.push(speedCtl)
 
     if (swapColumns) {
         setTimeout(swapColumnsCtl, 1000)
